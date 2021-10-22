598385f70f51fa6bb8ab1cedc0a858553a663c9c
dev-hugo-hash <vitor.hugoss100@gmail.com>
2021-10-22 17:12:45 -0300

    Criada segunda estrutura HTML, utilisando de javaScript para apresentar uma saudação

---
+++ Scripts/js01_estrutura.js
@@ -0,0 +1,14 @@
+  /*Capturar o nome de usuário e apresentyar uma saudaçãio para o mesmo.
+        Definir variável
+        El. Entrada: Prompt.
+        El. Saida: Write*/
+
+        /*Definição*/
+        var js_name;
+
+        /* Entrada */
+        js_name = window.prompt('Infome seu nome: ', 'Digite aqui');
+
+        /*Saida*/
+
+        document.write('Olá '+ js_name + 'Seja bem vindo!');
