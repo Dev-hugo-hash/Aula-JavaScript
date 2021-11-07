aebbd7b49b496190607f9ee68371ed8ecc692d9c
Dev-hugo-hash <vitor.hugoss100@gmail.com>
2021-11-06 22:40:22 -0300

    Estrutura html feita para pedir o nome do visitante e imprimir

---
+++ Scripts/js01_estrutura1.js
@@ -0,0 +1,13 @@
+//Captura nome do usuário para apresenta-lo 
+//Definicição de variável 
+//El. entrada : Prompt
+//El. saida:  write 
+
+//Definicição
+let js_name;
+
+//Entrada
+js_name = window.prompt('Informe o seu nome: ', 'Digite aqui');
+
+//saida 
+js_name = document.write('Olá! Seja bem vindo ' + js_name + '!');
